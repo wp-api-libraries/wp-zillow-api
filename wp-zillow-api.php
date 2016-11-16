11e4afbf53567acaa8639e91a7e203c4e5ee6f22
Brandon Hubbard <bkhubbard@gmail.com>
2016-11-16 13:28:59 -0800

    More Cleanup

--- wp-zillow-api.php
+++ wp-zillow-api.php
@@ -50,12 +50,12 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 */
 		protected $base_uri = 'https://www.zillow.com/webservice/';
 
-
 		/**
-		 * __construct function.
+		 * Construct.
 		 *
 		 * @access public
 		 * @param mixed $zws_id ZWSID.
+		 * @param mixed $output Output.
 		 * @return void
 		 */
 		public function __construct( $zws_id, $output = 'json' ) {
@@ -91,13 +91,11 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * Get Zillow Reviews (https://www.zillow.com/howto/api/ReviewsAPI.htm)
 		 *
 		 * @access public
-		 * @param mixed $zws_id The Zillow Web Service Identifier. Required.
-		 * @param mixed $screenname The screenname of the user whose reviews you would like to return. Required.
-		 * @param mixed $email The email of the user whose reviews you would like to return.
-		 * @param mixed $count The count of reviews you would like to return. If omitted, up to 3 are returned. 10 is maximum.
-		 * @param mixed $output The type of output desired. Specify 'xml' for XML output and 'json' for JSON output. If omitted, 'xml' is assumed.
-		 * @param mixed $return_team_member_reviews If value = 'true', API returns reviews written directly for the account requested, in addition to any reviews attributed to this account from its team members if applicable. If omitted, returned reviews reviews defaults to just those written directly for the account requested.
-		 * @return $response API Response.
+		 * @param mixed  $screenname Screenname.
+		 * @param mixed  $email (default: null) Email.
+		 * @param string $count (default: '3') Count.
+		 * @param mixed  $returnTeamMemberReviews (default: null) Return Team Member Reviews.
+		 * @return Request.
 		 */
 		function get_reviews( $screenname, $email = null, $count = '3', $returnTeamMemberReviews = null ) {
 
@@ -117,7 +115,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @access public
 		 * @param mixed  $state (default: null) State.
 		 * @param string $callback (default: '') Callback.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_rate_summary( $state = null, $callback = '' ) {
 
@@ -131,14 +129,13 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * Get Monthly Payments (https://www.zillow.com/howto/api/GetMonthlyPayments.htm)
 		 *
 		 * @access public
-		 * @param mixed $zws_id The Zillow Web Service Identifier. Required.
-		 * @param mixed $price The price of the property for which monthly payment data will be calculated. Required.
-		 * @param mixed $down The percentage of the total property price that will be placed as a down payment. If omitted, a 20% down payment is assumed. If the down payment is less than 20%, a monthly private mortgage insurance amount is specified for each returned loan type.
-		 * @param mixed $dollarsdown The dollar amount that will be placed as a down payment. This amount will be used for the down payment if the 'down' parameter is omitted. If the down payment is less than 20% of the purchase price, a monthly private mortgage insurance amount is specified for each returned loan type.
-		 * @param mixed $zip The ZIP code in which the property is located. If omitted, monthly property tax and hazard insurance data will not be returned.
-		 * @param mixed $output The type of output desired. Specify 'xml' for XML output and 'json' for JSON output. If omitted, 'xml' is assumed.
-		 * @param mixed $callback The name of the JavaScript callback function used to process the returned JSON data. If specified, the returned JSON will be wrapped in a function call with the specified function name. This parameter is intended for use with dynamic script tags. The callback function is only used for JSON output.
-		 * @return void
+		 * @param mixed $price Price.
+		 * @param mixed $down (default: null) Down.
+		 * @param mixed $dollarsdown (default: null) DollarsDown.
+		 * @param mixed $zip (default: null) Zip.
+		 * @param mixed $output (default: null) Output.
+		 * @param mixed $callback (default: null) Callback.
+		 * @return Request.
 		 */
 		function get_monthly_payments( $price, $down = null, $dollarsdown = null, $zip = null, $output = null, $callback = null ) {
 
@@ -160,7 +157,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @param mixed $address Address.
 		 * @param mixed $citystatezip City/State/Zip.
 		 * @param mixed $rentzestimate (default: null) Rent Zestimate.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_deep_search_results( $address, $citystatezip, $rentzestimate = null ) {
 
@@ -188,7 +185,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @param mixed  $zpid ZPID.
 		 * @param string $count (default: '5') Count.
 		 * @param bool   $rentzestimate (default: false) Rent Zestimate.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_deep_comps( $zpid, $count = '5', $rentzestimate = false ) {
 
@@ -214,7 +211,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 *
 		 * @access public
 		 * @param mixed $zpid ZPID.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_updated_property_details( $zpid ) {
 
@@ -241,7 +238,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @param mixed $address Address.
 		 * @param mixed $citystatezip City/State/Zip.
 		 * @param bool  $rentzestimate (default: false) Rent Zestimate.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_search_results( $address, $citystatezip, $rentzestimate = false ) {
 
@@ -268,7 +265,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 *
 		 * @access public
 		 * @param mixed $zpid ZPID.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_zestimate( $zpid ) {
 
@@ -297,7 +294,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @param string $width (default: '600') Width.
 		 * @param string $height (default: '300') Height.
 		 * @param string $chart_duration (default: '1year') Chart Duration.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_chart( $zpid, $unit_type, $width = '600', $height = '300', $chart_duration = '1year' ) {
 
@@ -324,7 +321,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 * @param mixed $zpid ZPID.
 		 * @param mixed $count Count.
 		 * @param bool  $rentzestimate (default: false) Rent Zestimate.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_comps( $zpid, $count, $rentzestimate = false ) {
 
@@ -349,7 +346,7 @@ if ( ! class_exists( 'ZillowAPI' ) ) {
 		 *
 		 * @access public
 		 * @param mixed $url URL.
-		 * @return void
+		 * @return Request.
 		 */
 		function get_zpid_from_url( $url ) {
 
